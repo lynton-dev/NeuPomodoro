5b688eab207d14797387ccb0f90284896e2a5030
lynton-dev <61889203+lynton-dev@users.noreply.github.com>
2023-04-10 11:01:38 -0600

    Initial Commit

---
+++ NeuPomodoro/NeuPomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuPomodoroApp.swift
+//  NeuPomodoro
+//
+//  Created by Lynton Schoeman on 2023-04-10.
+//
+
+import SwiftUI
+
+@main
+struct NeuPomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
